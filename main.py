43868310c5066738e2da30945af1fb511f887218
Deepak-A19 <155350005+Deepak-A19@users.noreply.github.com>
2024-04-25 09:26:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import PyPDF2
+import os
+from langchain_experimental.llms import ChatLlamaAPI
+import gradio as gr
+
+# Configure GenerativeAI (replace with your API key)
+from llamaapi import LlamaAPI
+
+# Replace 'Your_API_Token' with your actual API token
+llama = LlamaAPI("LL-QoWL9fXItXVpIn4aeAJrWF5ktefdgE5y6iBvV0sIbEgpd62qyKGbTi1fKkFfDnZh")
+model = ChatLlamaAPI(client=llama)
+
+
+def extract_text_from_pdf(file_path):
+    """Extracts text from a PDF file, handling potential access errors."""
+    try:
+        with open(file_path, 'rb') as pdf_file:
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            text = ""
+            for page_num in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_num]
+                text += page.extract_text()
+            return text
+    except (FileNotFoundError, PermissionError) as e:
+        print(f"Error: {e}. Please check the file path and permissions.")
+        return ""
+
+
+def create_study_plan(file_path_1, file_path_2):
+    """Extracts text from two PDF files, then starts conversation with LLM."""
+    extracted_text_1 = extract_text_from_pdf(file_path_1)
+    extracted_text_2 = extract_text_from_pdf(file_path_2)
+    
+    calendar_part = extracted_text_1.split("Syllabus Content", 1) if "Syllabus Content" in extracted_text_1 else (
+        extracted_text_1, "")
+    syllabus_part = extracted_text_2
+    
+    convo = model.invoke(
+        f"Academic Calendar Content \n {calendar_part} Syllabus Content\n {syllabus_part} Generate a study plan by analyzing the files. Provide a detailed schedule for each day with date and time, including specific chapter topics to cover and prioritizing important topics.")
+    return convo.content
+
+
+iface = gr.Interface(fn=create_study_plan, inputs=["file", "file"], outputs="text",
+                     title="Generate Study Plan from Two PDFs",
+                     description="Upload two PDFs - one for the academic calendar and one for the syllabus - and get a study plan generated.",
+                     allow_flagging=False)
+
+iface.launch(share=True)
